b97b2c8a760d212a297461c81c12341c76bfa610
Alvar Aronija <aronija_1@hotmail.com>
2018-03-05 18:28:08 +0100

    Custom table view cell

---
+++ WeatherApp/HeaderTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  HeaderTableViewCell.swift
+//  WeatherApp
+//
+//  Created by Alvar Aronija on 04/03/2018.
+//  Copyright © 2018 Alvar Aronija. All rights reserved.
+//
+
+import UIKit
+
+class HeaderTableViewCell: UITableViewCell {
+    // MARK: Properties
+    @IBOutlet weak var placesTitle: UILabel!
+    @IBOutlet weak var forecastTitle: UILabel!
+    @IBOutlet weak var tempLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
